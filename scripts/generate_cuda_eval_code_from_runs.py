f9660fcf92bd69c434470c1ac1c7e16ed57f6d9e
xiexinch <xiexinch@outlook.com>
2026-02-09 22:18:00 +0800

    --dev=fix

--- scripts/generate_cuda_eval_code_from_runs.py
+++ scripts/generate_cuda_eval_code_from_runs.py
@@ -80,7 +80,7 @@ def extract_kernel_code(
 
 
 # one-shot template for generate c++ entry code
-one_shot_template = Template(f"""You are a CUDA expert. Your task is to generate a c++ entry code for the given kernel code and torch::Tensor entry code. You should follow the following rules:
+one_shot_template = Template("""You are a CUDA expert. Your task is to generate a c++ entry code for the given kernel code and torch::Tensor entry code. You should follow the following rules:
 - You should use the original kernel code and torch::Tensor entry code as a reference.
 - You should generate the entry code that is compatible with the torch::Tensor entry code.
 - You should write the entry code with the original kernel code
@@ -92,9 +92,9 @@ one_shot_template = Template(f"""You are a CUDA expert. Your task is to generate
 
 You are given the following kernel code and torch::Tensor entry code:
 ```cpp
-${macro_code}
-${kernel_code}
-${entry_code}
+$macro_code
+$kernel_code
+$entry_code
 ```
 
 Your example entry code:
